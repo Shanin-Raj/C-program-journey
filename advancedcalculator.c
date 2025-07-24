a1e704533375cd637a2efe2b36cfe440677c3292
Shanin-Raj <shaninraj05@gmail.com>
2025-07-24 11:49:40 +0530

    updated advanced calculator

--- advancedcalculator.c
+++ advancedcalculator.c
@@ -7,7 +7,7 @@ int main() {
 
     // --- User Input ---
     printf("Enter an operator (+, -, *, /): ");
-    scanf(" %c", &operator); // Note the space before %c to consume any newline characters
+    scanf(" %c", &operator); // space before %c is to consume any newline characters
 
     printf("Enter the first number: ");
     scanf("%lf", &num1);
@@ -17,6 +17,7 @@ int main() {
 
     // --- The 'if-else if-else' Decision Block ---
     // The program checks these conditions one by one from the top.
+    // We can use switch functionality here, but for simplicity, we will use if-else statements.
 
     if (operator == '+') {
         result = num1 + num2;
@@ -41,3 +42,37 @@ int main() {
 
     return 0;
 }
+//for using switch functionality, we can use the following code
+ 
+    // --- NEW: The 'switch' Statement ---
+    // The switch statement evaluates the 'operator' variable.
+/*  switch(operator) {
+        // It then jumps to the 'case' that matches the value.
+        case '+':
+            result = num1 + num2;
+            printf("Result: %.2f\n", result);
+            break; // 'break' exits the switch statement.
+
+        case '-':
+            result = num1 - num2;
+            printf("Result: %.2f\n", result);
+            break;
+
+        case '*':
+            result = num1 * num2;
+            printf("Result: %.2f\n", result);
+            break;
+
+        case '/':
+            if (num2 == 0) {
+                printf("Error! Cannot divide by zero.\n");
+            } else {
+                result = num1 / num2;
+                printf("Result: %.2f\n", result);
+            }
+            break;
+
+        // The 'default' case runs if no other case matches.
+        default:
+            printf("Error! Invalid operator.\n");
+    }*/
